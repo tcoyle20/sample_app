4c15a581a56224c21bbb967d0a80041213159d14
Ted Coyle <ted.coyle@nutrigraphix.com>
2013-07-02 22:54:24 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e5aebf08130308c953f174df74c1820af279796a4654bac8eaa003cc4f03411252ec4501889604d4d489bf898adddc3f111a015775b44e5a9bd97d772e5f3670'
