bd69551ed36a13efae347fe6e187f276b1aef810
Ted Coyle <ted.coyle@nutrigraphix.com>
2013-07-03 22:25:48 -0400

    add automated testing and spork, sublime testing. full title helper

--- Gemfile
+++ Gemfile
@@ -7,7 +7,8 @@ gem 'rails', '3.2.13'
 
 group :development, :test do
 	gem 'sqlite3', '1.3.5'
-	gem 'rspec-rails', '2.11.0'
+	gem 'rspec-rails', '2.13.1'
+	gem 'guard-rspec', '1.2.1'
 end
 
 
@@ -27,6 +28,11 @@ gem 'jquery-rails', '2.0.2'
 group :test do
 	gem 'nokogiri', '1.5.10'
 	gem 'capybara', '1.1.2'
+	gem 'rb-fsevent', '0.9.1', :require => false
+  gem 'growl', '1.0.3'
+  gem 'guard-spork', '1.2.0'
+  gem 'childprocess', '0.3.6'
+  gem 'spork', '0.9.2'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,7 +14,21 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "#{base_title} | Home")
+                        :text => "#{base_title}")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "#{base_title} | About Us")
     end
   end
 end
